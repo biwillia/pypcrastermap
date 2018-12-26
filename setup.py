da4ddda561fffbd9379e845b3d4ac59b81a66472
Benjamin I. Williams <bwilliams@fopen.org>
2018-12-26 23:19:35 +0100

    added setup files

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name = 'pypcrastermap',
+  packages = ['pypcrastermap'],
+  version = '0.1',
+  description = 'Python library for reading and writing PCRaster map files (csf)',
+  author = 'Benjamin I. Williams',
+  author_email = '',
+  url = 'https://github.com/biwillia/pypcrastermap',
+  download_url = 'https://github.com/biwillia/pypcrastermap/archive/0.1.tar.gz',
+  keywords = ['pypcrastermap', 'pcraster', 'map', 'csf' ],
+  classifiers = [],
+  install_requires = [ ]
+)
